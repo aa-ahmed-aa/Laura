4da42b588ef46db7e07b11ce123d700caf14b0a6
Server <server@server.com>
2018-02-10 13:54:49 +0200

    adding pdf parser

---
+++ index.js
@@ -0,0 +1,8 @@
+exports.PdfReader = require("./node_modules/pdfreader/PdfReader");
+exports.Rule = require("./node_modules/pdfreader/Rule");
+exports.LOG = require("./node_modules/pdfreader/lib/LOG.js");
+exports.parseTable = require("./node_modules/pdfreader/lib/parseTable.js");
+exports.parseColumns = require("./node_modules/pdfreader/lib/parseColumns.js");
+exports.SequentialParser = require("./node_modules/pdfreader/lib/SequentialParser.js"); // experimental
+exports.TableParser = require("./node_modules/pdfreader/lib/TableParser.js");
+exports.ColumnsParser = require("./node_modules/pdfreader/lib/ColumnsParser.js");
